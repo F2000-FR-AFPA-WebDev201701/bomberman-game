b2d7231b4ff1fe56d315ff1437b3c75e2b25a63a
SamSuphy <florent.dervier@gmail.com>
2017-03-08 15:20:17 +0100

    premier commit createGame

--- src/AppBundle/Controller/BoardController.php
+++ src/AppBundle/Controller/BoardController.php
@@ -2,18 +2,17 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class BoardController extends Controller
-{
+class BoardController extends Controller {
+
     /**
-     * @Route("/do")
+     * @Route("/do", name="doBoard")
      */
-    public function doAction()
-    {
+    public function doAction() {
         return $this->render('AppBundle:Board:do.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
--- src/AppBundle/Entity/Board.php
+++ src/AppBundle/Entity/Board.php
@@ -94,4 +94,3 @@ class Board
         return $this->players;
     }
 }
-
